26aa1f3b543ee97fdb7dae4714180a927b7300b3
Mykhailo Nester <nsmisha.dev@gmail.com>
2020-11-02 13:33:23 +0200

    - update app class to default; - add new comments;

--- example/src/main/java/com/crowdin/platform/example/BaseActivity.kt
+++ example/src/main/java/com/crowdin/platform/example/BaseActivity.kt
@@ -1,22 +1,14 @@
 package com.crowdin.platform.example
 
 import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.crowdin.platform.Crowdin
 
 abstract class BaseActivity : AppCompatActivity() {
 
-    /**
-     * We should wrap the base context of our activities, which is better to put it
-     * on BaseActivity, so that we don't have to repeat it for all activities one-by-one.
-     */
     override fun attachBaseContext(newBase: Context) {
+        //  We should wrap the base context of our activities, which is better to put it
+        //  on BaseActivity, so that we don't have to repeat it for all activities one-by-one.
         super.attachBaseContext(Crowdin.wrapContext(newBase))
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        com.crowdin.crowdin_controls.onActivityResult(this, requestCode)
     }
 }
